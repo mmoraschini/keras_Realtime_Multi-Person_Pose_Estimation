739fece82dea25e5f7f9de9862ebf6d8fc27dbef
Marco <marco.mora88@gmail.com>
2019-08-22 18:09:13 +0200

    Mainly refacotring, comments and docstrings

--- processing.py
+++ processing.py
@@ -1,13 +1,29 @@
-import math
+"""
+Module to process images and extract body parts and subsets, i.e. groups of parts, ideally persons
+"""
 
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter
 import cv2
+from scipy.ndimage.filters import gaussian_filter
 
 import util
 
 
 def extract_parts(input_image, params, model, model_params):
+    """
+    This function uses a Neural Network model to recognise persons and their body parts inside an image
+
+    :param input_image: cv2 image to analyse
+    :param params: parameters of the algorithm read from the config file
+    :param model: keras weights
+    :param model_params: parameters of the model read from the config file
+    :return:
+        an ndarray where each line is a subset (ideally a person), the first 18 columns are the ids of the parts which
+            map to the corresponding line in the second retruned variable, the 19th is the confidence of the subset and
+            the last one is the number of valid parts for that subset
+        an ndarray where each line is a part, the first two columns the x and y of the part, the third column the
+            confidence of that part and the fourth the part type
+    """
     multiplier = [x * model_params['boxsize'] / input_image.shape[0] for x in params['scale_search']]
 
     # Body parts location heatmap, one per part (19)
@@ -41,11 +57,13 @@ def extract_parts(input_image, params, model, model_params):
         heatmap_avg = heatmap_avg + heatmap / len(multiplier)
         paf_avg = paf_avg + paf / len(multiplier)
 
+    # all_peaks is an array where each element contains an array of tuples. Each element of the tuple is a peak for
+    # a given body part
     all_peaks = []
     peak_counter = 0
 
-    for part in range(18):
-        hmap_ori = heatmap_avg[:, :, part]
+    for i in range(18):
+        hmap_ori = heatmap_avg[:, :, i]
         hmap = gaussian_filter(hmap_ori, sigma=3)
 
         # Find the pixel that has maximum value compared to those around it
@@ -61,10 +79,12 @@ def extract_parts(input_image, params, model, model_params):
         # reduce needed because there are > 2 arguments
         peaks_binary = np.logical_and.reduce(
             (hmap >= hmap_left, hmap >= hmap_right, hmap >= hmap_up, hmap >= hmap_down, hmap > params['thre1']))
+
+        # peaks will contain a tuple for each peak in the image
         peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0]))  # note reverse
-        peaks_with_score = [x + (hmap_ori[x[1], x[0]],) for x in peaks]  # add a third element to tuple with score
+        peaks_with_score = [x + (hmap_ori[x[1], x[0]],) for x in peaks]  # add a third element to tuples (the score)
         idx = range(peak_counter, peak_counter + len(peaks))
-        peaks_with_score_and_id = [peaks_with_score[i] + (idx[i],) for i in range(len(idx))]
+        peaks_with_score_and_id = [peaks_with_score[i] + (idx[i],) for i in range(len(idx))]  # add id to tuples
 
         all_peaks.append(peaks_with_score_and_id)
         peak_counter += len(peaks)
@@ -73,19 +93,21 @@ def extract_parts(input_image, params, model, model_params):
     special_k = []
     mid_num = 10
 
-    for k in range(len(util.hmapIdx)):
-        score_mid = paf_avg[:, :, [x - 19 for x in util.hmapIdx[k]]]
-        cand_a = all_peaks[util.limbSeq[k][0] - 1]
-        cand_b = all_peaks[util.limbSeq[k][1] - 1]
+    for k in range(len(util.limb_paf_idx)):
+        # PAF for the given limbs (i.e. connection between parts)
+        score_mid = paf_avg[:, :, [x - 19 for x in util.limb_paf_idx[k]]]
+        # get all peaks for the given parts
+        cand_a = all_peaks[util.part_seq[k][0] - 1]  # each element is [x, y, score, unique_id]
+        cand_b = all_peaks[util.part_seq[k][1] - 1]  # each element is [x, y, score, unique_id]
         n_a = len(cand_a)
         n_b = len(cand_b)
-        # index_a, index_b = util.limbSeq[k]
+        # index_a, index_b = util.part_seq[k]
         if n_a != 0 and n_b != 0:
             connection_candidate = []
             for i in range(n_a):
                 for j in range(n_b):
                     vec = np.subtract(cand_b[j][:2], cand_a[i][:2])
-                    norm = math.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
+                    norm = np.linalg.norm(vec)
                     # failure case when 2 body parts overlaps
                     if norm == 0:
                         continue
@@ -93,121 +115,98 @@ def extract_parts(input_image, params, model, model_params):
 
                     startend = list(zip(np.linspace(cand_a[i][0], cand_b[j][0], num=mid_num),
                                         np.linspace(cand_a[i][1], cand_b[j][1], num=mid_num)))
+                    startend = np.array(startend).round().astype(int)
 
-                    vec_x = np.array(
-                        [score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0]
-                         for I in range(len(startend))])
-                    vec_y = np.array(
-                        [score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1]
-                         for I in range(len(startend))])
+                    vec_x = np.array([score_mid[se_i[1], se_i[0], 0] for se_i in startend])
+                    vec_y = np.array([score_mid[se_i[1], se_i[0], 1] for se_i in startend])
 
                     score_midpts = np.multiply(vec_x, vec[0]) + np.multiply(vec_y, vec[1])
                     score_with_dist_prior = sum(score_midpts) / len(score_midpts) + min(
-                        0.5 * input_image.shape[0] / norm - 1, 0)
+                        0.5 * input_image.shape[0] / norm - 1, 0)  # mean of the points + penalty if segment is too big
                     criterion1 = len(np.nonzero(score_midpts > params['thre2'])[0]) > 0.8 * len(
-                        score_midpts)
+                        score_midpts)  # If more than 80% of midpoints have an high score
                     criterion2 = score_with_dist_prior > 0
                     if criterion1 and criterion2:
-                        connection_candidate.append([i, j, score_with_dist_prior,
-                                                     score_with_dist_prior + cand_a[i][2] + cand_b[j][2]])
+                        connection_candidate.append([i, j, score_with_dist_prior])
 
             connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
-            connection = np.zeros((0, 5))
-            for c in range(len(connection_candidate)):
-                i, j, s = connection_candidate[c][0:3]
-                if i not in connection[:, 3] and j not in connection[:, 4]:
-                    connection = np.vstack([connection, [cand_a[i][3], cand_b[j][3], s, i, j]])
-                    if len(connection) >= min(n_a, n_b):
+            # Will contain [seq_id_a, seq_id_b, score, ith_of_a, jth_of_b]
+            connections = np.zeros((0, 5))
+            for cc in connection_candidate:
+                i, j, s = cc
+                if i not in connections[:, 3] and j not in connections[:, 4]:
+                    connections = np.vstack([connections, [cand_a[i][3], cand_b[j][3], s, i, j]])
+                    if len(connections) >= min(n_a, n_b):
                         break
 
-            connection_all.append(connection)
+            connection_all.append(connections)
         else:
             special_k.append(k)
             connection_all.append([])
 
-    # last number in each row is the total parts number of that person
-    # the second last number in each row is the score of the overall configuration
-    subset = np.empty((0, 20))
-    candidate = np.array([item for sublist in all_peaks for item in sublist])
+    # Each subset is a person (or at least should be)
+    # The last element of each row is the total number of parts belonging to that person
+    # The second last element of each row is the score of the overall configuration
+    # Here you basically map each part to the person they belong to
+    subsets = np.empty((0, 20))
+    # Put together all rows of all_peaks and add the part type
+    candidates = np.array([item[:-1] + (i,) for i, sublist in enumerate(all_peaks) for item in sublist])
 
-    for k in range(len(util.hmapIdx)):
-        if k not in special_k:
-            part_as = connection_all[k][:, 0]
-            part_bs = connection_all[k][:, 1]
-            index_a, index_b = np.array(util.limbSeq[k]) - 1
+    del all_peaks
 
-            for i in range(len(connection_all[k])):  # = 1:size(temp,1)
-                found = 0
-                subset_idx = [-1, -1]
-                for j in range(len(subset)):  # 1:size(subset,1):
-                    if subset[j][index_a] == part_as[i] or subset[j][index_b] == part_bs[i]:
-                        subset_idx[found] = j
-                        found += 1
+    for k in range(len(util.limb_paf_idx)):
+        if k in special_k:
+            continue
 
-                if found == 1:
-                    j = subset_idx[0]
-                    if subset[j][index_b] != part_bs[i]:
-                        subset[j][index_b] = part_bs[i]
-                        subset[j][-1] += 1
-                        subset[j][-2] += candidate[part_bs[i].astype(int), 2] + connection_all[k][i][2]
-                elif found == 2:  # if found 2 and disjoint, merge them
-                    j1, j2 = subset_idx
-                    membership = ((subset[j1] >= 0).astype(int) + (subset[j2] >= 0).astype(int))[:-2]
-                    if len(np.nonzero(membership == 2)[0]) == 0:  # merge
-                        subset[j1][:-2] += (subset[j2][:-2] + 1)
-                        subset[j1][-2:] += subset[j2][-2:]
-                        subset[j1][-2] += connection_all[k][i][2]
-                        subset = np.delete(subset, j2, 0)
-                    else:  # as like found == 1
-                        subset[j1][index_b] = part_bs[i]
-                        subset[j1][-1] += 1
-                        subset[j1][-2] += candidate[part_bs[i].astype(int), 2] + connection_all[k][i][2]
+        connections = connection_all[k]
 
-                # if find no partA in the subset, create a new subset
-                elif not found and k < 17:
-                    row = -1 * np.ones(20)
-                    row[index_a] = part_as[i]
-                    row[index_b] = part_bs[i]
-                    row[-1] = 2
-                    row[-2] = sum(candidate[connection_all[k][i, :2].astype(int), 2]) + connection_all[k][i][2]
-                    subset = np.vstack([subset, row])
+        part_as = connections[:, 0]
+        part_bs = connections[:, 1]
+        index_a, index_b = util.part_seq[k] - 1
+
+        # For each connection of the limb
+        for i in range(connections.shape[0]):
+            found = 0
+            subset_idx = [-1, -1]
+            for j in range(subsets.shape[0]):
+                if subsets[j][index_a] == part_as[i] or subsets[j][index_b] == part_bs[i]:
+                    subset_idx[found] = j
+                    found += 1
+
+            if found == 1:
+                j = subset_idx[0]
+                if subsets[j][index_b] != part_bs[i]:
+                    subsets[j][index_b] = part_bs[i]
+                    subsets[j][-1] += 1
+                    subsets[j][-2] += candidates[part_bs[i].astype(int), 2] + connections[i][2]
+            elif found == 2:  # if found 2 and disjoint, merge them
+                j1, j2 = subset_idx
+                membership = ((subsets[j1] >= 0).astype(int) + (subsets[j2] >= 0).astype(int))[:-2]
+                if len(np.nonzero(membership == 2)[0]) == 0:  # merge
+                    subsets[j1][:-2] += (subsets[j2][:-2] + 1)
+                    subsets[j1][-2:] += subsets[j2][-2:]
+                    subsets[j1][-2] += connections[i][2]
+                    subsets = np.delete(subsets, j2, 0)
+                else:  # as like found == 1
+                    subsets[j1][index_b] = part_bs[i]
+                    subsets[j1][-1] += 1
+                    subsets[j1][-2] += candidates[part_bs[i].astype(int), 2] + connections[i][2]
+
+            # if find no partA in the subset, create a new subset
+            elif not found and k < 17:
+                row = np.full(20, fill_value=-1)
+                row[index_a] = part_as[i]  # sequential unique id of part a across persons
+                row[index_b] = part_bs[i]  # sequential unique id of part b across persons
+                row[-1] = 2
+                row[-2] = sum(candidates[connections[i, :2].astype(int), 2]) + connections[i][2]
+                subsets = np.vstack([subsets, row])
 
     # delete some rows of subset which has few parts occur
     delete_idx = []
-    for i in range(len(subset)):
-        if subset[i][-1] < 4 or subset[i][-2] / subset[i][-1] < 0.4:
+    for i in range(len(subsets)):
+        # Too few parts or low average score
+        if subsets[i][-1] < 4 or subsets[i][-2] / subsets[i][-1] < 0.4:
             delete_idx.append(i)
-    subset = np.delete(subset, delete_idx, axis=0)
+    subsets = np.delete(subsets, delete_idx, axis=0)
 
-    return all_peaks, subset, candidate
-
-
-def draw(input_image, all_peaks, subset, candidate, resize_fac=1):
-    canvas = input_image.copy()
-
-    for i in range(18):
-        for j in range(len(all_peaks[i])):
-            a = all_peaks[i][j][0] * resize_fac
-            b = all_peaks[i][j][1] * resize_fac
-            cv2.circle(canvas, (a, b), 2, util.colors[i], thickness=-1)
-
-    stickwidth = 4
-
-    for i in range(17):
-        for s in subset:
-            index = s[np.array(util.limbSeq[i]) - 1]
-            if -1 in index:
-                continue
-            cur_canvas = canvas.copy()
-            y = candidate[index.astype(int), 0]
-            x = candidate[index.astype(int), 1]
-            m_x = np.mean(x)
-            m_y = np.mean(y)
-            length = ((x[0] - x[1]) ** 2 + (y[0] - y[1]) ** 2) ** 0.5
-            angle = math.degrees(math.atan2(x[0] - x[1], y[0] - y[1]))
-            polygon = cv2.ellipse2Poly((int(m_y * resize_fac), int(m_x * resize_fac)),
-                                       (int(length * resize_fac / 2), stickwidth), int(angle), 0, 360, 1)
-            cv2.fillConvexPoly(cur_canvas, polygon, util.colors[i])
-            canvas = cv2.addWeighted(canvas, 0.4, cur_canvas, 0.6, 0)
-
-    return canvas
+    return subsets, candidates
